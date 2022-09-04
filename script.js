065bc5b7241870a8ac2f64262d682bf28d9718cd
sinahuss <hussaini.sina@gmail.com>
2022-09-04 07:53:06 -0700

    Add basic functions

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const buttons = document.querySelector('.buttons');
+var input;
 
-for(let i = 0; i <= 9; i++) {
+for(let i = 9; i >= 0; i--) {
     const button = document.createElement('button');
     button.innerText = `${i}`;
     buttons.appendChild(button);
+
+
 }
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operation, num1, num2) {
+    return operation(num1, num2);
+}
+
+buttons.addEventListener('click', e => console.log(e.target.innerText), true);
