a0952f5b7380f2b0759effa4e1f6f2c6c3ba9f78
sinahuss <hussaini.sina@gmail.com>
2022-09-05 17:17:56 -0700

    Add operations functions

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const buttons = document.querySelector('.buttons');
-const CE = document.getElementById('CE');
-const C = document.getElementById('C');
-const DEL = document.getElementById('DEL');
+const operations = document.querySelector('.operation');
 
 var mainDisplay = document.querySelector('.main');
 var previousDisplay = document.querySelector('.previous');
-var input;
+
+var reset = true;
+var phaseOne = true;
+var solved = false;
+
+var key;
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
@@ -23,30 +26,70 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+//Create Object literal to avoid using switch or long if/else statements while
+//allowing more actions to be added
+const actions = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+};
+
 function operate(operation, num1, num2) {
     return operation(num1, num2);
 }
 
-/* function updateMain(num) {
-    mainDisplay.innerText += num;
-} */
+/* equals.addEventListener('click', () => {
+    mainDisplay.innerText = mainDisplay.innerText
+}); */
 
 buttons.addEventListener('click', e => {
-    let num = e.target.innerText;
-    if (!isNaN(num) || num == '.') {
-        mainDisplay.innerText += num;
+    let button = e.target;
+
+    if (button.className == 'input') {
+
+        if (reset) {
+            mainDisplay.innerText = button.innerText;
+        }
+
+        /* else if (operatingSecond) {
+            mainDisplay.innerText = button.innerText;
+            operatingSecond = !operatingSecond;
+        } */
+        
+        else {
+            mainDisplay.innerText += button.innerText;
+        }
+
+        reset = !reset;
+    }
+
+    else if (button.className == 'operation') {
+        key = button.innerText;
+        //if ()
+        previousDisplay.innerText = mainDisplay.innerText + ' ' + key;
+
+        reset = !reset;
+    }
+
+    else if (button.id == 'equals') {
+        previousDisplay.innerText += ' ' + mainDisplay.innerText + ' =';
+        mainDisplay.innerText = operate(actions[key],
+                        mainDisplay.innerText,
+                        parseInt(previousDisplay.innerText));
+        
+    }
+    
+    else if (button.id == 'CE') {
+        mainDisplay.innerText = '';
+        previousDisplay.innerText = '';
+    }
+    
+    else if (button.id == 'C') {
+        mainDisplay.innerText = '';
+    }
+    
+    else if (button.id == 'DEL') {
+        console.log('del');
     }
 }, true);
-
-CE.addEventListener('click', () => {
-    mainDisplay.innerText = '';
-    previousDisplay.innerText = '';
-});
-
-C.addEventListener('click', () => {
-    mainDisplay.innerText = '';
-});
-
-DEL.addEventListener('click', () => {
-    mainDisplay.innerText = '';
-});
